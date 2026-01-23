b4ef30c90e305c6002b35eb528e4092fb7881259
Joel J Gerard <git@jjg.dev>
2026-01-22 21:39:50 -0800

    Add Reporter abstraction for verbosity-gated output

---
+++ tools/reporter.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+
+class VerbosityLevel(IntEnum):
+    NONE = 0
+    PROG = 1
+    INFO = 2
+
+
+class Reporter:
+    def __init__(self, verbosity: int):
+        if verbosity < 0:
+            raise ValueError("verbosity must be >= 0")
+
+        self.verbosity = verbosity
+
+    def report(self, verbosity_level: VerbosityLevel, message):
+        if self.verbosity >= verbosity_level:
+            print(message)
